57dcd367a179c08b0589e89511127c99f2ffcdd3
bhance79 <ivanr1520@gmail.com>
2023-09-16 16:48:39 -0700

    Diego change

--- ml23/ejercicio_3_NaiveBayes/naive_bayes.py
+++ ml23/ejercicio_3_NaiveBayes/naive_bayes.py
@@ -6,23 +6,15 @@ class NaiveBayes():
 
     def fit(self, X, y):
         # TODO: Calcula la probabilidad de que una muestra sea positiva P(y=1)
-<<<<<<< HEAD
         self.prior_positives = sum(y)/len(y)
 
         # TODO: Calcula la probabilidad de que una muestra sea negativa P(y=0)
         self.prior_negative = 1 - self.prior_positives
-=======
-        # self.prior_positives = 
-
-        # TODO: Calcula la probabilidad de que una muestra sea negativa P(y=0)
-        # self.prior_negative = 
->>>>>>> upstream/master
 
         # TODO: Para cada palabra del vocabulario x_i
         # calcula la probabilidad de: P(x_i| y=1)
         # Guardalas en un arreglo de numpy:
         # self._likelihoods_positives = [P(x_1| y=1), P(x_2| y=1), ..., P(x_n| y=1)]
-<<<<<<< HEAD
         self._likelihoods_positives = np.zeros(X.shape[1])
         self._likelihoods_negatives = np.zeros(X.shape[1])
 
@@ -34,35 +26,23 @@ class NaiveBayes():
 
 
         self._likelihoods_positives = (self._likelihoods_positives + self.alpha) / (sum(y == 1)+(X.shape[1] * self.alpha)) #sum(y==1)
-=======
-        # self._likelihoods_positives = 
->>>>>>> upstream/master
         
         # TODO:  Para cada palabra del vocabulario x_i, calcula P(x_i| y=0)
         # Guardalas en un arreglo de numpy:
         # self._likelihoods_negatives = [P(x_1| y=0), P(x_2| y=0), ..., P(x_n| y=0)]
 
-<<<<<<< HEAD
         self._likelihoods_negatives = (self._likelihoods_negatives + self.alpha) / (sum(y == 0)+(X.shape[1] * self.alpha))
-=======
-        # self._likelihoods_negatives = _likelihoods_negatives
->>>>>>> upstream/master
         return self
 
     def predict(self, X):
         # TODO: Calcula la distribución posterior para la clase 1 dado los nuevos puntos X
         # utilizando el prior y los likelihoods calculados anteriormente
         # P(y = 1 | X) = P(y=1) * P(x1|y=1) * P(x2|y=1) * ... * P(xn|y=1)
-<<<<<<< HEAD
         posterior_positive =  self.prior_positives * np.prod(self._likelihoods_positives[X == 1]) * np.prod(1 - self._likelihoods_positives[X == 0])
-=======
-        # posterior_positive = 
->>>>>>> upstream/master
 
         # TODO: Calcula la distribución posterior para la clase 0 dado los nuevos puntos X
         # utilizando el prior y los likelihoods calculados anteriormente
         # P(y = 0 | X) = P(y=0) * P(x1|y=0) * P(x2|y=0) * ... * P(xn|y=0)
-<<<<<<< HEAD
         posterior_negative = self.prior_negative * np.prod(self._likelihoods_negatives[X == 1]) * np.prod(1 - self._likelihoods_negatives[X == 0])
 
         # TODO: Determina a que clase pertenece la muestra X dado las distribuciones posteriores
@@ -72,13 +52,6 @@ class NaiveBayes():
             clase = 0
             
         return clase
-=======
-        # posterior_negative = 
-
-        # TODO: Determina a que clase pertenece la muestra X dado las distribuciones posteriores
-        # clase = 
-        return
->>>>>>> upstream/master
     
     def score(self, X, y):
         y_pred = self.predict(X)
